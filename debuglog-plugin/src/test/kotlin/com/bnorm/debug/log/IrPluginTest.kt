b24711813044b8d5cb6f81909fcedd0e8c42e8b3
Lachlan McKee <lachlandevelopment@gmail.com>
2021-01-01 19:07:19 +0000

    Inlined 'testIrPlugin'

--- debuglog-plugin/src/test/kotlin/com/bnorm/debug/log/IrPluginTest.kt
+++ debuglog-plugin/src/test/kotlin/com/bnorm/debug/log/IrPluginTest.kt
@@ -64,105 +64,94 @@ fun doSomething() {
 
   @Test
   fun `IR plugin enabled`() {
-    testIrPlugin(
-      isPluginEnabled = true,
-      expectedGreetMethod = """
-        public static final String greet(@NotNull final String greeting, @NotNull final String name) {
-            Intrinsics.checkNotNullParameter(greeting, "greeting");
-            Intrinsics.checkNotNullParameter(name, "name");
-            System.out.println((Object)("⇢ greet(greeting=" + greeting + ", name=" + name + ')'));
-            final TimeMark markNow = TimeSource.Monotonic.INSTANCE.markNow();
-            try {
-                if (!Intrinsics.areEqual(greeting, "Hello")) {
-                    System.out.println((Object)("⇠ greet [" + (Object)Duration.toString-impl(markNow.elapsedNow-UwyO8pc()) + "] = " + "Early return"));
-                    return "Early return";
-                }
-                Thread.sleep(15L);
-                final String string = greeting + ", " + name + '!';
-                System.out.println((Object)("⇠ greet [" + (Object)Duration.toString-impl(markNow.elapsedNow-UwyO8pc()) + "] = " + string));
-                return string;
-            }
-            catch (Throwable t) {
-                System.out.println((Object)("⇠ greet [" + (Object)Duration.toString-impl(markNow.elapsedNow-UwyO8pc()) + "] = " + t));
-                throw t;
-            }
-        }
-    """.trimIndent(),
+    val result = compile(sourceFile = main, DebugLogComponentRegistrar(true))
+    assertEquals(KotlinCompilation.ExitCode.OK, result.exitCode)
 
-      expectedDoSomethingMethod = """
-        public static final void doSomething() {
-            System.out.println((Object)"⇢ doSomething()");
-            final TimeMark markNow = TimeSource.Monotonic.INSTANCE.markNow();
-            try {
-                Thread.sleep(15L);
-                System.out.println((Object)("⇠ doSomething [" + (Object)Duration.toString-impl(markNow.elapsedNow-UwyO8pc()) + ']'));
-            }
-            catch (Throwable t) {
-                System.out.println((Object)("⇠ doSomething [" + (Object)Duration.toString-impl(markNow.elapsedNow-UwyO8pc()) + "] = " + t));
-                throw t;
-            }
-        }
-    """.trimIndent(),
+    val javaCode = result.javaCode("MainKt")
 
-      outputVerifyFunc = { out ->
-        assert(out.size == 8)
-        assert(out[0] == "⇢ greet(greeting=Hello, name=World)")
-        assert(out[1].matches("⇠ greet \\[\\d+(\\.\\d+)?ms] = Hello, World!".toRegex()))
-        assert(out[2] == "Hello, World!")
-        assert(out[3] == "⇢ greet(greeting=Hello, name=Kotlin IR)")
-        assert(out[4].matches("⇠ greet \\[\\d+(\\.\\d+)?ms] = Hello, Kotlin IR!".toRegex()))
-        assert(out[5] == "Hello, Kotlin IR!")
-        assert(out[6] == "⇢ doSomething()")
-        assert(out[7].matches("⇠ doSomething \\[\\d+(\\.\\d+)?ms]".toRegex()))
+    assertFunction(javaCode, "public static final String greet",
+      """
+      public static final String greet(@NotNull final String greeting, @NotNull final String name) {
+          Intrinsics.checkNotNullParameter(greeting, "greeting");
+          Intrinsics.checkNotNullParameter(name, "name");
+          System.out.println((Object)("⇢ greet(greeting=" + greeting + ", name=" + name + ')'));
+          final TimeMark markNow = TimeSource.Monotonic.INSTANCE.markNow();
+          try {
+              if (!Intrinsics.areEqual(greeting, "Hello")) {
+                  System.out.println((Object)("⇠ greet [" + (Object)Duration.toString-impl(markNow.elapsedNow-UwyO8pc()) + "] = " + "Early return"));
+                  return "Early return";
+              }
+              Thread.sleep(15L);
+              final String string = greeting + ", " + name + '!';
+              System.out.println((Object)("⇠ greet [" + (Object)Duration.toString-impl(markNow.elapsedNow-UwyO8pc()) + "] = " + string));
+              return string;
+          }
+          catch (Throwable t) {
+              System.out.println((Object)("⇠ greet [" + (Object)Duration.toString-impl(markNow.elapsedNow-UwyO8pc()) + "] = " + t));
+              throw t;
+          }
       }
-    )
+      """.trimIndent())
+
+    assertFunction(javaCode, "public static final void doSomething",
+      """
+      public static final void doSomething() {
+          System.out.println((Object)"⇢ doSomething()");
+          final TimeMark markNow = TimeSource.Monotonic.INSTANCE.markNow();
+          try {
+              Thread.sleep(15L);
+              System.out.println((Object)("⇠ doSomething [" + (Object)Duration.toString-impl(markNow.elapsedNow-UwyO8pc()) + ']'));
+          }
+          catch (Throwable t) {
+              System.out.println((Object)("⇠ doSomething [" + (Object)Duration.toString-impl(markNow.elapsedNow-UwyO8pc()) + "] = " + t));
+              throw t;
+          }
+      }
+      """.trimIndent())
+
+    val out = invokeMain(result, "MainKt").trim().split("""\r?\n+""".toRegex())
+    assert(out.size == 8)
+    assert(out[0] == "⇢ greet(greeting=Hello, name=World)")
+    assert(out[1].matches("⇠ greet \\[\\d+(\\.\\d+)?ms] = Hello, World!".toRegex()))
+    assert(out[2] == "Hello, World!")
+    assert(out[3] == "⇢ greet(greeting=Hello, name=Kotlin IR)")
+    assert(out[4].matches("⇠ greet \\[\\d+(\\.\\d+)?ms] = Hello, Kotlin IR!".toRegex()))
+    assert(out[5] == "Hello, Kotlin IR!")
+    assert(out[6] == "⇢ doSomething()")
+    assert(out[7].matches("⇠ doSomething \\[\\d+(\\.\\d+)?ms]".toRegex()))
   }
 
   @Test
   fun `IR plugin disabled`() {
-    testIrPlugin(
-      isPluginEnabled = false,
-      expectedGreetMethod = """
-        public static final String greet(@NotNull final String greeting, @NotNull final String name) {
-            Intrinsics.checkNotNullParameter(greeting, "greeting");
-            Intrinsics.checkNotNullParameter(name, "name");
-            if (!Intrinsics.areEqual(greeting, "Hello")) {
-                return "Early return";
-            }
-            Thread.sleep(15L);
-            return greeting + ", " + name + '!';
-        }
-      """.trimIndent(),
-
-      expectedDoSomethingMethod = """
-        public static final void doSomething() {
-            Thread.sleep(15L);
-        }
-    """.trimIndent(),
-
-      outputVerifyFunc = { out ->
-        assertTrue(out.size == 2)
-        assertTrue(out[0] == "Hello, World!")
-        assertTrue(out[1] == "Hello, Kotlin IR!")
-      }
-    )
-  }
-
-  private fun testIrPlugin(
-    isPluginEnabled: Boolean,
-    expectedGreetMethod: String,
-    expectedDoSomethingMethod: String,
-    outputVerifyFunc: (List<String>) -> Unit
-  ) {
-    val result = compile(sourceFile = main, DebugLogComponentRegistrar(isPluginEnabled))
+    val result = compile(sourceFile = main, DebugLogComponentRegistrar(false))
     assertEquals(KotlinCompilation.ExitCode.OK, result.exitCode)
 
-    result.javaCode("MainKt").also { javaCode ->
-      assertFunction(javaCode, "public static final String greet", expectedGreetMethod)
-      assertFunction(javaCode, "public static final void doSomething", expectedDoSomethingMethod)
-    }
+    val javaCode = result.javaCode("MainKt")
 
-    outputVerifyFunc(invokeMain(result, "MainKt").trim().split("""\r?\n+""".toRegex()))
+    assertFunction(javaCode, "public static final String greet",
+      """
+      public static final String greet(@NotNull final String greeting, @NotNull final String name) {
+          Intrinsics.checkNotNullParameter(greeting, "greeting");
+          Intrinsics.checkNotNullParameter(name, "name");
+          if (!Intrinsics.areEqual(greeting, "Hello")) {
+              return "Early return";
+          }
+          Thread.sleep(15L);
+          return greeting + ", " + name + '!';
+      }
+      """.trimIndent())
+
+    assertFunction(javaCode, "public static final void doSomething",
+      """
+      public static final void doSomething() {
+          Thread.sleep(15L);
+      }
+      """.trimIndent())
+
+    val out = invokeMain(result, "MainKt").trim().split("""\r?\n+""".toRegex())
+    assertTrue(out.size == 2)
+    assertTrue(out[0] == "Hello, World!")
+    assertTrue(out[1] == "Hello, Kotlin IR!")
   }
 
   private fun assertFunction(javaCode: String, functionStatement: String, expectedFunction: String) {
